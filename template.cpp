802fccefb0e2cf751c8fb733ad90c57187a370ef
avnithv <63022387+avnithv@users.noreply.github.com>
2023-05-21 21:40:52 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vt vector
+#define pb push_back
+#define f first
+#define s second
+#define endl "\n"
+#define mp make_pair
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define TC int tc; cin >> tc; while (tc--) solve();
+#define FORN(i, a, b) for(int i = a; i < b; i++)
+#define EACH(x, a) for(auto x : a)
+#define DBG(z, n) forn(i,0,n)cerr<<z[i]<<" \n"[i==n-1];
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+
+signed main() {
+  return 0;
+}
