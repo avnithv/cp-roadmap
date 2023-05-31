32e25de740471acb351151f89c6e289400721ce5
avnithv <63022387+avnithv@users.noreply.github.com>
2023-05-30 21:21:37 -0400

    Update mod.cpp

--- templates/mod.cpp
+++ templates/mod.cpp
@@ -5,22 +5,13 @@ using namespace std;
 #define MOD(x) mod = (x);
 
 ll mod = 1e9+7;
-struct M {
-    ll x;
-    M (ll xx) : x((xx + mod) % mod) {}
-    M pow(M b) {
-        if (b == M(0)) return 1;
-        M z = pow(b / 2); z = z * z;
-        return (b.x & 1 ? (*this) * z : z);
-    }
-    M inv() {return pow(mod - 2);}
-    M operator+(M o) {return M((x + o.x) % mod); }
-    M operator-(M o) {return M((x - o.x + mod) % mod);}
-    M operator*(M o) {return M((x * o.x) % mod);}
-    M operator/(M o) {return (*this) * o.inv();}
-    bool operator==(M o) {return x == o.x;}
-    bool operator<(M o) {return x < o.x;}
-    bool operator<=(M o) {return x <= o.x;}
-    bool operator>(M o) {return x > o.x;}
-    bool operator>=(M o) {return x >= o.x;}
-};
+
+ll pow(ll &a, ll &b) {
+    if (!b) return 1;
+    ll x = pow(a, b/2);
+    return ((x * x) % mod* (b & 1 ? a : 1)) % mod;
+}
+
+ll inv(ll x) {
+    return pow(x, mod - 2);
+}
