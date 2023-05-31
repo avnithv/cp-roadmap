f6addf6ee9ff7322ac0977cdfc41bb8f21645358
avnithv <63022387+avnithv@users.noreply.github.com>
2023-05-30 21:35:53 -0400

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -12,6 +12,8 @@ using namespace std;
 #define FORN(i, a, b) for(int i = a; i < b; i++)
 #define EACH(x, a) for(auto x : a)
 #define DBG(z, n) FORN(i,0,n)cerr<<z[i]<<" \n"[i==n-1];
+#define DBG2(z, n) EACH(zzz, z) {DBG(zzz, n)}
+#define SET(arr, val) memset((arr), (val), (sizeof(arr)));
 
 // only in emergencies
 // #define int ll
@@ -20,6 +22,8 @@ typedef long long ll;
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pl;
 typedef unordered_set<int> usi;
+
+const ll mod = 1e9+7;
 
 signed main() {
   return 0;
