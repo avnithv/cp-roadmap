85d71e571f3cb82294222f018aa5cd3adecc7992
avnithv <63022387+avnithv@users.noreply.github.com>
2023-06-04 20:58:22 -0400

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define vt vector
@@ -10,10 +11,13 @@ using namespace std;
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define TC int tc; cin >> tc; while (tc--) solve();
 #define FORN(i, a, b) for(int i = a; i < b; i++)
-#define EACH(x, a) for(auto x : a)
+#define EACH(x, a) for(auto &x : a)
 #define DBG(z, n) FORN(i,0,n)cerr<<z[i]<<" \n"[i==n-1];
 #define DBG2(z, n) EACH(zzz, z) {DBG(zzz, n)}
 #define SET(arr, val) memset((arr), (val), (sizeof(arr)));
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define log(x) (63-__builtin_clzll(x))
 
 // only in emergencies
 // #define int ll
