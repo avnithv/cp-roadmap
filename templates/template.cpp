22edbbbcad2c6079c7956459f1422ce1bd44f63a
avnithv <63022387+avnithv@users.noreply.github.com>
2023-06-07 10:03:43 -0400

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define vt vector
+
 #define pb push_back
 #define f first
 #define s second
@@ -12,20 +12,24 @@ using namespace std;
 #define TC int tc; cin >> tc; while (tc--) solve();
 #define FORN(i, a, b) for(int i = a; i < b; i++)
 #define EACH(x, a) for(auto &x : a)
-#define DBG(z, n) FORN(i,0,n)cerr<<z[i]<<" \n"[i==n-1];
-#define DBG2(z, n) EACH(zzz, z) {DBG(zzz, n)}
+#define DBG(z) FORN(i,0,sz(z)cerr<<z[i]<<" \n"[i==sz(z)-1];
+#define DBG2(z) EACH(zzz, z) {DBG(zzz)}
 #define SET(arr, val) memset((arr), (val), (sizeof(arr)));
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 #define log(x) (63-__builtin_clzll(x))
+#define tcT template<class T
+#define tcTU tcT, class U
 
 // only in emergencies
 // #define int ll
 
+tctT> using vt<T> = vector<T>;
+tctTU> using gp<T, U> = gp_hash_table<T, U>;
+
 typedef long long ll;
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pl;
-typedef unordered_set<int> usi;
 
 const ll mod = 1e9+7;
 
