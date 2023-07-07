6fbd554915019f109d1aa57491ec483885eb746a
avnithv <63022387+avnithv@users.noreply.github.com>
2023-07-06 20:19:31 -0400

    Update hashes.cpp

--- templates/hashes.cpp
+++ templates/hashes.cpp
@@ -4,16 +4,38 @@
 using namespace std;
 using namespace __gnu_pbds;
 
+using ll = long long;
+using str = string;
+template<class T> using vt = vector<T>;
+
+#define FORN(i,a,b) for(int i = a; i < b; i++)
+#define sz(a) (int)a.size()
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+#define RAND(a, b) uniform_int_distribution<ll>(a, b)(rng)
+
 /* ========================================================================== */
 /* ========================================================================== */
 
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-
-struct chash {
-	const uint64_t C = ll(4e18 * acos(0)) | 71;
-	ll operator()(ll x) const { return __builtin_bswap64((x^RANDOM)*C); }
-};
-
-struct phash {
-    int operator()(pi x) const { return (x.s* 31 + x.s) ^ RANDOM; }
+struct Hash {
+	int n;
+	const ll M = (1ULL<<61) - 1, B = RAND(0, M - 1);
+	vt<ll> pow, hsh;
+	ll mod_mul(ll a, ll b) {
+		ll res = 0; a %= M;
+		while (b) {
+			if (b & 1) res = (res + a) % M;
+			a = (2 * a) % M;
+			b >>= 1;
+		}
+		return res;
+	}
+	//ll mod_mul(ll a, ll b) { return ((__int128)a*b) % M; }
+	Hash(str s) {
+		n = sz(s); pow.resize(n+1); pow[0] = 1;
+		FORN(i,0,n) pow[i+1] = mod_mul(pow[i], B);
+		hsh.resize(n+1); hsh[0] = 0;
+		FORN(i,0,n) hsh[i+1] = (mod_mul(hsh[i], B) + s[i]) % M;
+	}
+	ll hash(int a, int b) { return (hsh[b+1] - mod_mul(hsh[a], pow[b-a+1]) + M) % M; }
 };
