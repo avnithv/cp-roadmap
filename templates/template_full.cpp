25a3e1394ee0301a3c409b249a10d5fabad00fdf
avnithv <63022387+avnithv@users.noreply.github.com>
2023-07-11 09:27:28 -0400

    Update template_full.cpp

--- templates/template_full.cpp
+++ templates/template_full.cpp
@@ -44,19 +44,36 @@ const ll MXN = 1e5+5;
 /*=======================================================================*/
 /*============================NUMBER_THEORY==============================*/
 
-inline ll mpw(ll a, ll b) {
-    if (!b) return 1;
-    ll x = mpw(a, b/2);
-    return ((x * x) % MOD) * (b & 1 ? a : 1) % MOD;
-}
 
-inline ll inv(ll x) {
-    return mpw(x, MOD - 2);
-}
-
-inline ll norm(ll x) {
-    return (x % MOD + MOD) % MOD;
-}
+template<int MOD> struct Modular {
+	static const int mod = MOD;
+	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+	Modular():v(0) {}
+	Modular(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD); if (v < 0) v += MOD; }
+	bool operator==(const Modular& o) const { return v == o.v; }
+	friend bool operator!=(const Modular& a, const Modular& b) { return !(a == b); }
+	friend bool operator<(const Modular& a, const Modular& b) { return a.v < b.v; }
+	Modular& operator+=(const Modular& o) { if ((v += o.v) >= MOD) v -= MOD; return *this; }
+	Modular& operator-=(const Modular& o) { if ((v -= o.v) < 0) v += MOD; return *this; }
+	Modular& operator*=(const Modular& o) { v = int((ll)v*o.v%MOD); return *this; }
+	Modular& operator/=(const Modular& o) { return (*this) *= inv(o); }
+	friend Modular mpw(Modular a, ll p) {
+		Modular ans = 1; 
+		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans; }
+	friend Modular inv(const Modular& a) { return mpw(a,MOD-2); }
+	Modular operator-() const { return Modular(-v); }
+	Modular& operator++() { return *this += 1; }
+	Modular& operator--() { return *this -= 1; }
+	friend Modular operator+(Modular a, const Modular& b) { return a += b; }
+	friend Modular operator-(Modular a, const Modular& b) { return a -= b; }
+	friend Modular operator*(Modular a, const Modular& b) { return a *= b; }
+	friend Modular operator/(Modular a, const Modular& b) { return a /= b; }
+	friend istream& operator>>(istream& inp, Modular& a) { ll x; cin >> x; a = Modular(x); return inp;}
+	friend ostream& operator<<(ostream& out, const Modular& a) { out << a.v; return out; }
+};
+using Mint = Modular<MOD>;
+using pM = pair<Mint, Mint>;
 
 vt<ll> fc(MXN+1), iv(MXN+1);
 void precompute_factorials() {
