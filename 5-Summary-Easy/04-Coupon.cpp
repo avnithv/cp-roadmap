0f0c5258ea16424524ed5cc13e17b847a14cc6e4
avnithv <63022387+avnithv@users.noreply.github.com>
2023-07-11 13:23:12 -0400

    Create 04-Coupon.cpp

---
+++ 5-Summary-Easy/04-Coupon.cpp
@@ -0,0 +1,72 @@
+/* https://atcoder.jp/contests/abc246/tasks/abc246_c */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pb push_back
+#define f first
+#define s second
+#define endl "\n"
+#define mp make_pair
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define TC int tc; cin >> tc; while (tc--) solve();
+#define FORN(i, a, b) for(int i = a; i < b; i++)
+#define REV(i, b, a) for(int i = b - 1; i >= a; i--)
+#define EACH(x, a) for(auto &x : a)
+#define DBG(z) FORN(i,0,sz(z))cerr<<z[i]<<" \n"[i==sz(z)-1];
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define log(x) (63-__builtin_clzll(x))
+#define tcT template<class T
+#define tcTU tcT, class U
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+#define rand(a, b) uniform_int_distribution<ll>(a, b)(rng)
+
+// only in emergencies
+#define int ll
+
+using ll = long long;
+using str = string;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+tcT> using vt = vector<T>;
+tcTU> using gp = gp_hash_table<T, U>;
+tcT> using pq = priority_queue<T>;
+tcT> using qu = queue<T>;
+
+tcT> inline void chmin(T &a, T b) {a = min(a, b);}
+tcT> inline void chmax(T &a, T b) {a = max(a, b);}
+
+void solve() {
+	
+}
+
+signed main() {
+	FASTIO
+
+	ll n, k, x; cin >> n >> k >> x;
+	vt<ll> arr(n);
+	EACH(z, arr) cin >> z;
+
+	sort(all(arr));
+
+	REV(i,n,0) {
+		while (k && arr[i] >= x) {
+			arr[i] -= x;
+			k--;
+		}
+	}
+
+	ll ans = 0;
+	EACH(z, arr) ans += z;
+
+	if (k) {
+		sort(all(arr));
+		REV(i,n,max(0LL, n-k)) ans -= arr[i];
+	}
+	cout << ans << endl;
+}
