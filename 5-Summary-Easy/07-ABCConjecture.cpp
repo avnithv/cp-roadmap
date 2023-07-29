f3c48384cec91c9e7f6d8aec6ceabda229bf6f11
avnithv <63022387+avnithv@users.noreply.github.com>
2023-07-29 16:10:43 -0400

    Create 07-ABCConjecture.cpp

---
+++ 5-Summary-Easy/07-ABCConjecture.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define f first
+#define s second
+#define endl "\n"
+#define mp make_pair
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define TC int tc; cin >> tc; while (tc--) solve();
+#define FORN(i,a,b) for(int i=a; i<b; i++)
+#define REV(i,b,a) for(int i=b-1; i>=a; i--)
+#define EACH(x, a) for (auto &x: a)
+#define DBG(z) FORN(i,0,sz(z))cerr<<z[i]<<" \n"[i==sz(z)-1];
+#
+#define sz(a) (int)a.size()
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define lb lower_bound
+#define ub upper_bound
+
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+template<class T> using vt = vector<T>;
+
+template<class T> void chmin(T &a, T b) {a = min(a, b);}
+template<class T> void chmax(T &a, T b) {a = max(a, b);}
+
+const ll MXN = 1e5+5, MOD = 1e9+7;
+
+signed main() {
+	FASTIO
+	ll n; cin >> n;
+
+	ll ans = 0;
+	ll a=1;
+	while (a*a*a <= n) {
+		ll b=a;
+		while (b*b <= n/a) {
+			ans += (n/a)/b - b + 1;
+			b++;
+		}
+		a++;
+	}
+	cout << ans << endl;
+}
