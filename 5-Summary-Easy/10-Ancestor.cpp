d81de1b72ed3c1a6259ea30b6cde41cb58b2cf0b
avnithv <63022387+avnithv@users.noreply.github.com>
2023-07-29 16:32:20 -0400

    Create 10-Ancestor.cpp

---
+++ 5-Summary-Easy/10-Ancestor.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define f first
+#define s second
+#define endl "\n"
+#define mp make_pair
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define TC int tc; cin >> tc; while (tc--) solve();
+#define FORN(i,a,b) for(int i=a; i<b; i++)
+#define REV(i,b,a) for(int i=b-1; i>=a; i--)
+#define EACH(x, a) for (auto &x: a)
+#define DBG(z) FORN(i,0,sz(z))cerr<<z[i]<<" \n"[i==sz(z)-1];
+
+#define sz(a) (int)a.size()
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define lb lower_bound
+#define ub upper_bound
+
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+template<class T> using vt = vector<T>;
+
+template<class T> void chmin(T &a, T b) {a = min(a, b);}
+template<class T> void chmax(T &a, T b) {a = max(a, b);}
+
+const ll MXN = 1e5+5, MOD = 1e9+7;
+
+signed main() {
+	FASTIO
+	int n; cin >> n;
+	vt<int> a(n, -1);
+	FORN(i,1,n) cin >> a[i];
+
+	int i = n-1, ans = 0;
+	while (a[i] != -1) {
+		i = a[i]-1;
+		ans++;
+	}
+	cout << ans << endl;
+}
