c8bf1ba4d7010695fe4e573a2b2a447fc6157fbe
avnithv <63022387+avnithv@users.noreply.github.com>
2023-07-31 11:44:54 -0400

    Create 01-ChatBan.cpp

---
+++ 4-Binary-Search/01-ChatBan.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define f first
+#define s second
+#define endl "\n"
+#define mp make_pair
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define TC int tc; cin >> tc; while (tc--) solve();
+#define FORN(i,a,b) for(int i=a; i<b; i++)
+#define REV(i,b,a) for(int i=b-1; i>=a; i--)
+#define EACH(x, a) for (auto &x: a)
+#define DBG(z) FORN(i,0,sz(z))cerr<<z[i]<<" \n"[i==sz(z)-1];
+
+#define sz(a) (int)a.size()
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define lb lower_bound
+#define ub upper_bound
+
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+template<class T> using vt = vector<T>;
+
+template<class T> void chmin(T &a, T b) {a = min(a, b);}
+template<class T> void chmax(T &a, T b) {a = max(a, b);}
+
+const ll MXN = 4e6+1, MOD = 1e9+7;
+
+bool pos(ll k, ll n, ll x) {
+    ll ans = 0;
+    if (n >= k) {
+        ans += k * (k + 1) / 2;
+        ans += k * (k - 1) / 2;
+        ans -= (2*k-n) * (2*k-n-1) / 2;
+    } else {
+        ans += n * (n + 1) / 2;
+    }
+    return ans < x;
+}
+
+void solve() {
+    ll k, x; cin >> k >> x;
+    ll l = 0, r = 2*k-1;
+    while (r-l>1) {
+        ll m = (l+r)/2;
+        if (pos(k,m,x)) l=m;
+        else r=m;
+    }
+    cout << l+1 << endl;
+}
+
+signed main() {
+	FASTIO
+    TC
+}
