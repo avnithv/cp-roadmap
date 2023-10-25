d93d391d93398471936319460fe87734484b61d6
avnithv <63022387+avnithv@users.noreply.github.com>
2023-10-25 19:02:33 -0400

    Create lazy_update_AM_query_M.cpp

---
+++ templates/segment_trees/lazy_update_AM_query_M.cpp
@@ -0,0 +1,62 @@
+tcT> struct LazySegmentTree {
+    int len;
+    vt<T> tree, lazy;
+    // CHANGE THESE
+    static constexpr T TREE_DEFAULT = 0;
+    static constexpr T LAZY_DEFAULT = 0;
+    
+    LazySegmentTree(int n) {
+        len = 1; while (len < n) len *= 2;
+        tree.resize(2 * len, TREE_DEFAULT);
+        lazy.resize(2 * len, LAZY_DEFAULT);
+    }
+
+    void propagate(int x, int lx, int rx) {
+        if (rx - lx == 1) return;
+        lazy_merged(lazy[2 * x + 1], lazy[x]);
+        lazy_merged(lazy[2 * x + 2], lazy[x]);
+        lazy_merged(tree[2 * x + 1], lazy[x]);
+        lazy_merged(tree[2 * x + 2], lazy[x]);
+        lazy[x] = LAZY_DEFAULT;
+    }
+
+    void add(int l, int r, T v, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if (lx >= r || rx <= l) return;
+        if (lx >= l && rx <= r) {
+            lazy_merged(lazy[x], v);
+            lazy_merged(tree[x], v);
+            return;
+        }
+        int m = (lx + rx) / 2;
+        add(l, r, v, 2 * x + 1, lx, m);
+        add(l, r, v, 2 * x + 2, m, rx);
+        tree[x] = tree_merge(tree[2 * x + 1], tree[2 * x + 2]);
+    }
+
+    T query(int l, int r, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if (lx >= r || rx <= l) return TREE_DEFAULT;
+        if (lx >= l && rx <= r) return tree[x];
+        int m = (lx + rx) / 2;
+        int left = query(l, r, 2 * x + 1, lx, m);
+        int right = query(l, r, 2 * x + 2, m, rx);
+        return tree_merge(left, right);
+    }
+
+    void add(int l, int r, T v) { add(l, r, v, 0, 0, len); }
+    T query(int l, int r) { return query(l, r, 0, 0, len); }
+    void tree_merged(T &a, T b) { a = tree_merge(a, b); }
+    void lazy_merged(T &a, T b) { a = lazy_merge(a, b); }
+
+    // max, min
+    T tree_merge(T a, T b) {
+        return max(a, b);
+    }
+
+    // add, min, max
+    T lazy_merge(T a, T b) {
+        return a + b;
+    }
+};
+using SegTree = LazySegmentTree<ll>;
