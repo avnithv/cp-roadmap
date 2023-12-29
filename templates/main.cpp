a60ef2cb56da728880ad648514fb03ecbba0be23
avnithv <63022387+avnithv@users.noreply.github.com>
2023-12-29 02:46:29 -0500

    Update main.cpp

--- templates/main.cpp
+++ templates/main.cpp
@@ -1,41 +1,29 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 #define pb push_back
 #define f first
 #define s second
 #define endl "\n"
-#define mp make_pair
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define TC int tc; cin >> tc; while (tc--) solve();
 #define FORN(i, a, b) for(int i = a; i < b; i++)
 #define REV(i, b, a) for(int i = b - 1; i >= a; i--)
 #define EACH(x, a) for(auto &x : a)
-#define DBG(z) FORN(lopital,0,sz(z))cerr<<z[lopital]<<" \n"[lopital==sz(z)-1];
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 #define rall(a) (a).rbegin(), (a).rend()
-#define log(x) (63-__builtin_clzll(x))
+#define log(x) (63-__builtin_clzll(ll(x)))
 #define tcT template<class T
 #define tcTU tcT, class U
 #define lb lower_bound
 #define ub upper_bound
 // #define int ll
 using ll = long long;
-using pi = pair<int, int>;
-using pl = pair<ll, ll>;
-using ppl = pair<pl, pl>;
 tcT> using vt = vector<T>;
 tcT> using pq = priority_queue<T>;
-tcT> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-tcT> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-tcTU> using ordered_map = tree<T, U, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-tcTU> using ordered_multimap = tree<T, U, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 tcT> inline void chmin(T &a, T b) {a = min(a, b);}
 tcT> inline void chmax(T &a, T b) {a = max(a, b);}
-const ll MOD = 998244353, MXN = 1e5+5, INF = 1e15;
+const ll MOD = 998244353, MXN = 2e5+5, INF = 1e16;
 
 void solve() {
 
