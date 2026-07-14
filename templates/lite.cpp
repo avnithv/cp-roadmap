2cbbcbdf9f88043ad6357a2b104b50b01b81c79d
Avnith Vijayram <63022387+avnithv@users.noreply.github.com>
2026-07-13 22:02:40 -0400

    Add lite.cpp with basic C++ template structure

---
+++ templates/lite.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template<class T> using V = vector<T>;
+#define FORN(i,a,b) for (int i = a; i < b; i++) 
+#define REV(i,b,a) for (int i = b-1; i >= a; i--)
+#define EACH(x,a) for (auto x : a)
+#define all(x) (x).begin(), (x).end()
+using pi = pair<int, int>;
+#define F first
+#define S second
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+}
